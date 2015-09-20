6f97f1597a8a194ebb00a0d1ac8764af47bdd920
megc84 <megc84@gmail.com>
2015-09-20 19:09:50 -0400

    Second test

---
+++ colors.py
@@ -0,0 +1,16 @@
+import random
+
+
+colors = ['red', 'blue', 'green', 'yellow', 'cyan'];
+print (colors[0])
+print ( len(colors))
+
+for c in colors:
+	print ("Nice " +  c)
+
+print (random.randint(0,9))
+
+
+for d in range(random.randint(2,9)):
+
+		print ("Me imprimo " + d + " veces")
